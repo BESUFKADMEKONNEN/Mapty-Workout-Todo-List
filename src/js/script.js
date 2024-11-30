d841f5d441d277fe9191c12f1e057d22e95f4d1d
Besu Mack <163390472+BESUFKADMEKONNEN@users.noreply.github.com>
2024-11-30 16:02:46 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -106,7 +106,7 @@ class APP {
     this.#map = L.map('map').setView(coord, 13);
 
     //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
